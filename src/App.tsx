3a8081991b2346e3af912ee576c2c7dd41e23c21
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-01-28 11:57:48 +0300

    Canvas Board

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import './App.css';
 import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-// import CanvasBoard from "./components/CanvasBoard";
+import CanvasBoard from "./components/CanvasBoard";
 // import ScoreCard from "./components/ScoreCard";
 import store from "./store";
 
@@ -13,7 +13,7 @@ function App() {
             <Container maxW="container.lg" centerContent>
                 <Heading as="h1" size="xl">SNAKE GAME</Heading>
         {/*        <ScoreCard />*/}
-        {/*        <CanvasBoard height={600} width={1000} />*/}
+                <CanvasBoard height={600} width={1000} />
             </Container>
         </ChakraProvider>
     </Provider>
