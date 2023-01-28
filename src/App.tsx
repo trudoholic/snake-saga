3d8623e0cba7eb2fb5a1577781984c61ba85ac4b
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-01-28 11:47:47 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+// import CanvasBoard from "./components/CanvasBoard";
+// import ScoreCard from "./components/ScoreCard";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+        <ChakraProvider>
+            <Container maxW="container.lg" centerContent>
+                <Heading as="h1" size="xl">SNAKE GAME</Heading>
+        {/*        <ScoreCard />*/}
+        {/*        <CanvasBoard height={600} width={1000} />*/}
+            </Container>
+        </ChakraProvider>
+    </Provider>
   );
 }
 
